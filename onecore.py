c1fb3286843cb4149c99d2832a847ee644fb7625
Kate Groppe <kgroppe@gmail.com>
2023-12-04 14:46:38 -0600

    added hashing function

--- onecore.py
+++ onecore.py
@@ -12,5 +12,29 @@ PW_HIGH = 8
 
 print("Processing Single Core")
 print(os.getcwd())
-print("Password Caracter Set: ", chars)
-print("Password Lengths: ", str(PW_LOW), " - ", str(PW_HIGH))
+print("Password Caracter Set:", chars)
+print("Password Lengths:", str(PW_LOW), "-", str(PW_HIGH))
+
+startTime = time.time()
+
+try:
+    fp = open('PW-ALL', 'w')
+except:
+    print("File Processing Error")
+    sys.exit(0)
+
+pwCount = 0
+
+for r in range(PW_LOW, PW_HIGH + 1):
+    for s in itertools.product(chars, repeat = r):
+        pw=''.join(s)
+        try:
+            sha256Hash = hashlib.sha256()
+            sha256Hash.update(SALT + pw)
+            sha256Digest = sha256Hash.hexdigest()
+            fp.write(sha256Digest+' '+pw+'\n')
+            pwCount += 1
+            del sha256Hash
+        except:
+            print("File Processing Error")
+
