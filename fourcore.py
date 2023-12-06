0e90afd7511a78c87676fa49a880fedb643f34a9
Kate Groppe <kgroppe@gmail.com>
2023-12-06 10:57:12 -0600

    starting forcore code

---
+++ fourcore.py
@@ -0,0 +1,14 @@
+import hashlib
+import time
+import os
+import itertools
+import multiprocessing
+
+chars = ['a','b','c','d','e','f','g','h']
+
+SALT = "&45Bvx9"
+PW_LOW = 4
+PW_HIGH = 8
+
+def pwGenerator():
+    pass
